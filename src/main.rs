7029e8472940a9c8b3a10075acf38130c0419caa
tjjfvi <tjjfvi@tjjfvi.com>
2021-09-08 16:36:54 -0700

    use include("lib.rs") in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,6 @@
-mod apply_thin_move;
-mod cube;
-mod cube_iter;
-mod display_cube;
-mod flip;
-mod is_solved;
-mod r#move;
-mod move_str;
-mod parse_cube;
-mod pos;
-mod scramble;
-mod slice;
-mod solve;
-mod solve_front_face;
-mod solve_inner_corners;
-mod solve_inner_cross;
-mod solve_inner_edges;
-mod solve_inner_shell;
-mod solve_step;
-mod swap_axes;
-mod value;
+include!("lib.rs");
 
 use std::io::{stdin, Read};
-
-pub use apply_thin_move::*;
-pub use cube::*;
-pub use cube_iter::*;
-pub use flip::*;
-pub use is_solved::*;
-pub use move_str::*;
-pub use parse_cube::*;
-pub use pos::*;
-pub use r#move::*;
-pub use scramble::*;
-pub use slice::*;
-pub use solve::*;
-pub use solve_front_face::*;
-pub use solve_inner_corners::*;
-pub use solve_inner_cross::*;
-pub use solve_inner_edges::*;
-pub use solve_inner_shell::*;
-pub use solve_step::*;
-pub use swap_axes::*;
-pub use value::*;
 
 fn main() {
   let mut input = String::new();
